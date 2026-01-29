702ad26a7548f7180977a9ab8c23796daa4c5d4d
Kazoo <yxchia98@gmail.com>
2026-01-29 06:52:09 +0000

    fixed guide

--- prep-democenter/setup_script-h100.sh
+++ prep-democenter/setup_script-h100.sh
@@ -100,7 +100,7 @@ echo "Copying Workshop Guides..."
 cp -R ../workshop-democenter/lab1-deploying-nims.html \
     ../workshop-democenter/lab2-running-nim-with-lora-adapter.html \
     ../workshop-democenter/lab3-deploy-rag.html \
-    $LOCAL_PEFT_DIRECTORY
+    ~/Desktop
 
 # Step 10: Make environment variables persistent
 echo "Saving environment variables to ~/.bashrc..."
